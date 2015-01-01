c74bdfcc939ab88d00c0b282d248238370bb051d
Brian Langenberger <bjl@usa.net>
2015-01-01 10:27:29 -0600

    Update copyright text for the new year

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'DVD-Audio Library'
-copyright = u'2014, Brian Langenberger'
+copyright = u'2015, Brian Langenberger'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
--- src/mlp.h
+++ src/mlp.h
@@ -1,6 +1,6 @@
 /********************************************************
  DVD-A Library, a module for reading DVD-Audio discs
- Copyright (C) 2014  Brian Langenberger
+ Copyright (C) 2014-2015  Brian Langenberger
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
